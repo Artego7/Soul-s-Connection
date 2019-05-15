a482905297336de0ddcc01ccfe96c89a3ffbb067
Artego <37409439+Artego7@users.noreply.github.com>
2019-05-15 21:07:28 +0200

    Update

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public List<GameObject> ItemList = new List<GameObject>();
+
+
+    void OptionSelectedMainMenu()
+    {
+        ItemList[0].SetActive(true);
+        ItemList[1].SetActive(true);
+        //ItemList[2].SetActive(true);
+        //ItemList[3].SetActive(true);
+        //ItemList[4].SetActive(true);
+        //ItemList[5].SetActive(true);
+        //ItemList[0].SetActive(false);
+        //ItemList[1].SetActive(false);
+        //ItemList[2].SetActive(false);
+        //ItemList[3].SetActive(false);
+        //ItemList[4].SetActive(false);
+        //ItemList[5].SetActive(false);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}

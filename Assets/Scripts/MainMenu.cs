080f41c07926940c73791e430d949d4f93da360b
Artego7 <abelzambrano98@gmail.com>
2019-05-16 14:11:22 +0200

    Update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,24 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    public List<GameObject> ItemList = new List<GameObject>();
-
-
-    void OptionSelectedMainMenu()
-    {
-        ItemList[0].SetActive(true);
-        ItemList[1].SetActive(true);
-        //ItemList[2].SetActive(true);
-        //ItemList[3].SetActive(true);
-        //ItemList[4].SetActive(true);
-        //ItemList[5].SetActive(true);
-        //ItemList[0].SetActive(false);
-        //ItemList[1].SetActive(false);
-        //ItemList[2].SetActive(false);
-        //ItemList[3].SetActive(false);
-        //ItemList[4].SetActive(false);
-        //ItemList[5].SetActive(false);
-    }
+    
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
---
+++ Assets/Scripts/OnMouseOverButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnMouseOverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+
+    public GameObject Soul1;
+    public GameObject Soul2;
+
+    private void Start()
+    {
+        Soul1.SetActive(false);
+        Soul2.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Soul1.SetActive(true);
+        Soul2.SetActive(true);
+
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        Soul1.SetActive(false);
+        Soul2.SetActive(false);
+    }
+}
